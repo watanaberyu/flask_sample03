e5d1889740933b2bfd05d00443a7ee0d940ad2e7
watanabe ryu <ryusoccer.0418@gmail.com>
2019-08-01 22:06:59 +0900

    フォームに入力した文字を出力できるようにした

--- 01.py
+++ 01.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/tweet", methods=["GET", "POST"])
+def tweet():
+    if request.method == "GET":
+        return render_template("index.html")
+
+    if request.method == "POST":
+        content = request.form["content"]
+        return render_template("index.html", content=content)
 
 
 if __name__ == "__main__":
